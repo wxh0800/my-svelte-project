c7d7ddb476cdfd2c09ca2fa8d2f03a72815d7ed3
William Wang <wxh0800@gmail.com>
2022-05-22 13:41:17 -0400

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,40 @@
+<script>
+	export let name;
+	let age = 18
+	let count = 0
+	$: doubled = count *2
+	function handleClick() {
+		count += 1
+	}
+</script>
+
+<main>
+	<h1>Hello {name}! {age}</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<button on:click={handleClick}>
+		Clicked {count} {count===1?'time':'times'}
+	</button>
+	<p>{count} doubled is {doubled}</p>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
